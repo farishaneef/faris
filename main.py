b041acc2f746ddd7dd0b1d0f14b8069969d8ac18
farishaneef <133044687+farishaneef@users.noreply.github.com>
2023-05-12 14:16:40 +0300

    Update pxt.json, main.blocks, main.ts, main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+turnoff = 0
+
+def on_forever():
+    global turnoff
+    if turnoff == 0 and ModuleWorld_Digital.button(ModuleWorld_Digital.mwDigitalNum.P0P1,
+        ModuleWorld_Digital.enButton.PRESS):
+        basic.show_string("ON")
+        ModuleWorld_PWM.RGB2(ModuleWorld_PWM.enColor.WHITE)
+        turnoff = 1
+    else:
+        if turnoff == 1 and ModuleWorld_Digital.button(ModuleWorld_Digital.mwDigitalNum.P0P1,
+            ModuleWorld_Digital.enButton.PRESS):
+            basic.show_string("OFF")
+            ModuleWorld_PWM.RGB2(ModuleWorld_PWM.enColor.OFF)
+            turnoff = 0
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,26 +1,27 @@
-input.onGesture(Gesture.Shake, function () {
-    basic.showLeds(`
-        . # # # #
-        . # # # #
-        . # # . #
-        # # . # #
-        . . . . .
-        `)
-    music.playMelody("C5 B A G F E D C ", 327)
-    basic.showLeds(`
-        . # # # #
-        . # # # #
-        . # # . #
-        # # . # #
-        . . . . .
-        `)
+let turnoff = 0
+input.onSound(DetectedSound.Loud, function () {
+    if (turnoff == 0) {
+        basic.showString("ON")
+        ModuleWorld_PWM.RGB2(ModuleWorld_PWM.enColor.White)
+        turnoff = 1
+    } else {
+        if (turnoff == 1) {
+            basic.showString("OFF")
+            ModuleWorld_PWM.RGB2(ModuleWorld_PWM.enColor.OFF)
+            turnoff = 0
+        }
+    }
 })
 basic.forever(function () {
-    basic.showLeds(`
-        . # . # .
-        # # # # #
-        # # # # #
-        . # # # .
-        . . # . .
-        `)
+    if (turnoff == 0 && ModuleWorld_Digital.Button(ModuleWorld_Digital.mwDigitalNum.P0P1, ModuleWorld_Digital.enButton.Press)) {
+        basic.showString("ON")
+        ModuleWorld_PWM.RGB2(ModuleWorld_PWM.enColor.White)
+        turnoff = 1
+    } else {
+        if (turnoff == 1 && ModuleWorld_Digital.Button(ModuleWorld_Digital.mwDigitalNum.P0P1, ModuleWorld_Digital.enButton.Press)) {
+            basic.showString("OFF")
+            ModuleWorld_PWM.RGB2(ModuleWorld_PWM.enColor.OFF)
+            turnoff = 0
+        }
+    }
 })
